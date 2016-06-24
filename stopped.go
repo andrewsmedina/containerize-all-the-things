ab0354397bf857f8ebe9045bf5a0ba170113c5bb
Andrews Medina <andrewsmedina@gmail.com>
2016-06-23 23:08:40 -0300

    add slide about how to list stopped containers

---
+++ stopped.go
@@ -0,0 +1,6 @@
+$ docker ps -a
+CONTAINER ID        IMAGE                COMMAND                  CREATED             STATUS                        PORTS                     NAMES
+3b5bdc3785bd        busybox              "/bin/sh -c 'while tr"   15 minutes ago      Exited (137) 12 minutes ago                             evil_mestorf
+ab4c619bf897        busybox              "/bin/sh"                18 minutes ago      Exited (0) 17 minutes ago                               awesome_sinoussi
+08e17bae0d20        busybox              "sh"                     18 minutes ago      Exited (0) 18 minutes ago                               kickass_snyder
+8a29663dbcd1        busybox              "echo caipyra"           22 minutes ago      Exited (0) 22 minutes ago                               backstabbing_wescoff
