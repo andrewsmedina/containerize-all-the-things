ac0a0c940eb6f112dbe29546d6eeb08d3bc8a8b0
Andrews Medina <andrewsmedina@gmail.com>
2016-06-24 17:57:42 -0300

    add slides about status

---
+++ stats.go
@@ -0,0 +1,4 @@
+$ docker stats f98ea7d0213e
+
+CONTAINER           CPU %               MEM USAGE / LIMIT     MEM %               NET I/O               BLOCK I/O           PIDS
+f98ea7d0213e        0.02%               12.31 MB / 1.044 GB   1.18%               3.237 kB / 3.095 kB   0 B / 0 B           1
